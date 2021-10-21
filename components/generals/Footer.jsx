633843a80a245e4c1acf366584e70126ada9496a
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-10-21 01:39:48 -0300

    add footer again

---
+++ components/generals/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+import Script from "next/script";
+
+export const Footer = () => {
+  return (
+    <>
+      <Script
+        src="https://kit.fontawesome.com/afd6aa68df.js"
+        strategy="lazyOnload"
+      />
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/devrebeleza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="i-icon fab fa-github"></i>
+        </a>
+        <a
+          href="https://github.com/devrebeleza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © 2021 Copyright: devReBeleza
+        </a>
+        <a
+          href="https://www.linkedin.com/in/renzogarcia/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="i-icon fab fa-linkedin"></i>
+        </a>
+      </footer>
+    </>
+  );
+};
