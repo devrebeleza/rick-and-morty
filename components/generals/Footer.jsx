b0cd5f574c6736704c50f3c11826de212d5c9387
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-12-24 12:17:42 -0300

    portfolio link

--- components/generals/Footer.jsx
+++ components/generals/Footer.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import styles from "../../styles/Home.module.css";
-import Script from "next/script";
+import React from 'react';
+import styles from '../../styles/Home.module.css';
+import Script from 'next/script';
 
 export const Footer = () => {
   return (
     <>
       <Script
-        src="https://kit.fontawesome.com/afd6aa68df.js"
-        strategy="lazyOnload"
+        src='https://kit.fontawesome.com/afd6aa68df.js'
+        strategy='lazyOnload'
       />
       <footer className={styles.footer}>
         <a
-          href="https://github.com/devrebeleza"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://github.com/devrebeleza'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          <i className="i-icon fab fa-github"></i>
+          <i className='i-icon fab fa-github'></i>
         </a>
         <a
-          href="https://github.com/devrebeleza"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://renzogarcia.vercel.app/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           © 2021 Copyright devReBeleza
         </a>
         <a
-          href="https://www.linkedin.com/in/renzogarcia/"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.linkedin.com/in/renzogarcia/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          <i className="i-icon fab fa-linkedin"></i>
+          <i className='i-icon fab fa-linkedin'></i>
         </a>
       </footer>
     </>
